b8efb9f7af4daa557c43a2441b8a0886252f0a26
abricoshka <top_acdemy_student@07-08>
2025-10-24 17:19:44 +0700

    feat: add circle animation

--- VisiblityConverter.cs
+++ VisiblityConverter.cs
@@ -38,4 +38,17 @@ namespace ai
             throw new NotImplementedException();
         }
     }
+
+    public class ThinkingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString() == "" ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
